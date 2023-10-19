47be79dfbae53d1d74c0550344a57bbe38fbdf5f
Roy Gardner <roybowesgardner@gmail.com>
2023-10-19 10:33:33 +0100

    Update streamlit_trial.py

--- streamlit_trial.py
+++ streamlit_trial.py
@@ -222,6 +222,7 @@ pp_graph = pp_data_dict['pp_graph']['graph']
 node_colors = pp_data_dict['pp_graph']['node_colors']
 display_graph(pp_graph,node_colors)
 
+
 #Query vertices using depth-first search
 #results_dict = query_graph(pp_graph,query_vertices=['CON_20','CON_21'],operator='AND',depth=1)
 #display_graph(results_dict['graph'],results_dict['node_colors'])
