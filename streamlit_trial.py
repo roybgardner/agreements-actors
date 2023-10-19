5a5fa135fb84856df9d93cea0f68374e96959ac6
Roy Gardner <roybowesgardner@gmail.com>
2023-10-19 10:36:24 +0100

    Update streamlit_trial.py

--- streamlit_trial.py
+++ streamlit_trial.py
@@ -228,6 +228,16 @@ display_graph(pp_graph,node_colors)
 #display_graph(results_dict['graph'],results_dict['node_colors'])
 
 #radio button to select operator type
+with st.sidebar.form("my_form"):
+   st.write("Inside the form")
+   slider_val = st.slider("Form slider")
+   checkbox_val = st.checkbox("Form checkbox")
+
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Submit")
+   if submitted:
+       st.write("slider", slider_val, "checkbox", checkbox_val)
+
 
 st.sidebar.write(" # Query the network")
 
