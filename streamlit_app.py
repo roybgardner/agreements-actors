c6743e25f2927c3f7cd3f7e6d8458fdf7077665d
Roy Gardner <roybowesgardner@gmail.com>
2024-01-16 11:42:41 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -270,9 +270,9 @@ st.write('Signatory data: Niamh Henry and Sanja Badanjak')
 st.write('Analysis/coding: Roy Gardner') 
 
 st.subheader('Approach') 
-st.write('Analysis of signatory data based on binary-valued relation matrices. Includes:') 
-st.write('1. Simple extraction of peace process data for network generation.') 
-st.write('2. Querying of peace process networks.') 
+st.write('Analysis of signatory data based on binary-valued relation matrices (BVRMs). Includes:') 
+st.write('1. Extraction BVRMs containing data from individual peace processes.') 
+st.write('2. Generation of adjacency matrices for querying and displaying peace process graphs.')  
 st.write('3. Generation of co-occurrence networks measuring, for example, a)\
           the number of agreements to which a pair of actors are co-signatories, b)\
           the number of signatories a pair of agreements have in common.\
@@ -299,4 +299,44 @@ st.write('Number of actors in peace process:',pp_data_dict['pp_matrix'].shape[1]
 adj_matrix,adj_vertices = binary_to_adjacency(pp_data_dict)
 display_networkx_graph(adj_matrix,range(0,len(adj_vertices)),adj_vertices,data_dict)
 
+#Query vertices using depth-first search
+with st.form("query"):
+    st.subheader("Query peace process network")
+    st.write('X/UI for formulating queries and providing users with insight into peace process actors and agreements.')
+    st.write('Select actors (in alpha order) with agreements (in date order) using the selectors below.')
+    st.write('Mixing and matching actor and agreements is supported but might not be sensible.')
+ 
+    # Get actors in alpha order
+    actor_options = [(vertex_id,data_dict['vertices_dict'][vertex_id][5]) for vertex_id in pp_data_dict['pp_actor_ids']]
+    actor_options = sorted(actor_options,key=lambda t:t[1])
+    actor_options = [t[0] + ': ' + t[1] for t in actor_options]
+
+    # Get agreements in date order
+    agreement_options = [(vertex_id,data_dict['vertices_dict'][vertex_id][5],data_dict['dates_dict'][vertex_id]) for vertex_id in pp_data_dict['pp_agreement_ids']]
+    agreement_options = sorted(agreement_options,key=lambda t:t[2])
+    agreement_options = [t[0] + ': ' + t[1] for t in agreement_options]
+
+    options_actor = st.multiselect(
+    'Select zero or more actors.',
+    actor_options,
+    [])
+
+    options_agreement = st.multiselect(
+    'Select zero or more agreements',
+    agreement_options,
+    [])
+
+    operator=["AND", "OR"]
+    select_operator=st.radio("Select operator", operator, index=0, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, horizontal=False, captions=None, label_visibility="visible")
+    depth=st.slider("Select depth", min_value=1, max_value=3, value=1, step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+
+# Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        options = [v.split(':')[0] for v in options_actor]
+        options.extend([v.split(':')[0] for v in options_agreement])
+
+        #query_indices = [adj_vertices.index(vertex) for vertex in query_vertices]
+        query_matrix,found_indices = get_query_matrix(options,adj_matrix,max_depth=depth,operator=operator)
+        display_networkx_graph(query_matrix,found_indices,adj_vertices,data_dict)
 
