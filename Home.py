ddb85777bb5738299e049fbe705e30bf9a417c3c
Roy Gardner <roybowesgardner@gmail.com>
2024-03-11 16:58:42 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,6 +4,14 @@ from streamlit_shared import *
 
 
 
+if "data_dict" not in st.session_state:
+    st.session_state["data_dict"] = {}
+if "pp_data_dict" not in st.session_state:
+    st.session_state["pp_data_dict"] = {}
+if "adj_matrix" not in st.session_state:
+    st.session_state["adj_matrix"] = {}
+if "adj_vertices" not in st.session_state:
+    st.session_state["adj_vertices"] = {}      
 
 # *********************************************************************************************************************
 
@@ -43,20 +51,13 @@ nodes_file = 'node_table.csv'
 links_file = 'links_table.csv'
 agreements_dict = 'agreements_dict.json'
 
-if "data_dict" not in st.session_state:
-    st.session_state["data_dict"] = {}
     
-if len(st.session_state["data_dict"]) == 0:
-    data_dict = load_agreement_actor_data(nodes_file,links_file,agreements_dict,data_path)
-else:
+if len(st.session_state["data_dict"]) > 0:
     data_dict = st.session_state["data_dict"]
+else:
+    data_dict = load_agreement_actor_data(nodes_file,links_file,agreements_dict,data_path)
 
-if "pp_data_dict" not in st.session_state:
-    st.session_state["pp_data_dict"] = {}
-if "adj_matrix" not in st.session_state:
-    st.session_state["adj_matrix"] = {}
-if "adj_vertices" not in st.session_state:
-    st.session_state["adj_vertices"] = {}          
+ 
 
 st.header('Credits') 
 st.write('Signatory data: Niamh Henry and Sanja Badanjak') 
