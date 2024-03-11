2ab4bd3ff1ca20a025a5cb6f1c06ec53445471ff
Roy Gardner <roybowesgardner@gmail.com>
2024-03-11 17:45:37 +0000

    Update 2_Query_Peace_Process.py

--- pages/2_Query_Peace_Process.py
+++ pages/2_Query_Peace_Process.py
@@ -40,44 +40,47 @@ st.markdown("""
 st.markdown('<p class="maintitle">Signatories Network Analysis</p>', unsafe_allow_html=True)
 st.header("Query a Peace Process Network")
 
+if len(st.session_state["pp_data_dict"]) > 0:
 
-#Query vertices using depth-first search
-with st.form("query"):
-    st.write('Interface for formulating queries and providing users with insight into peace process actors and agreements.')
-    st.write('Select actors (in alpha order) with agreements (in date order) using the selectors below.')
-    st.write('Mixing and matching actor and agreements is supported but might not be sensible.')
- 
-    # Get actors in alpha order
-    actor_options = [(vertex_id,data_dict['vertices_dict'][vertex_id][5]) for vertex_id in pp_data_dict['pp_actor_ids']]
-    actor_options = sorted(actor_options,key=lambda t:t[1])
-    actor_options = [t[0] + ': ' + t[1] for t in actor_options]
+    #Query vertices using depth-first search
+    with st.form("query"):
+        st.write('Interface for formulating queries and providing users with insight into peace process actors and agreements.')
+        st.write('Select actors (in alpha order) with agreements (in date order) using the selectors below.')
+        st.write('Mixing and matching actor and agreements is supported but might not be sensible.')
+    
+        # Get actors in alpha order
+        actor_options = [(vertex_id,data_dict['vertices_dict'][vertex_id][5]) for vertex_id in pp_data_dict['pp_actor_ids']]
+        actor_options = sorted(actor_options,key=lambda t:t[1])
+        actor_options = [t[0] + ': ' + t[1] for t in actor_options]
 
-    # Get agreements in date order
-    agreement_options = [(vertex_id,data_dict['vertices_dict'][vertex_id][5],data_dict['dates_dict'][vertex_id]) for vertex_id in pp_data_dict['pp_agreement_ids']]
-    agreement_options = sorted(agreement_options,key=lambda t:t[2])
-    agreement_options = [t[0] + ': ' + t[1] for t in agreement_options]
+        # Get agreements in date order
+        agreement_options = [(vertex_id,data_dict['vertices_dict'][vertex_id][5],data_dict['dates_dict'][vertex_id]) for vertex_id in pp_data_dict['pp_agreement_ids']]
+        agreement_options = sorted(agreement_options,key=lambda t:t[2])
+        agreement_options = [t[0] + ': ' + t[1] for t in agreement_options]
 
-    options_actor = st.multiselect(
-    'Select zero or more actors.',
-    actor_options,
-    [])
+        options_actor = st.multiselect(
+        'Select zero or more actors.',
+        actor_options,
+        [])
 
-    options_agreement = st.multiselect(
-    'Select zero or more agreements',
-    agreement_options,
-    [])
+        options_agreement = st.multiselect(
+        'Select zero or more agreements',
+        agreement_options,
+        [])
 
-    operator=["AND", "OR"]
-    select_operator=st.radio("Select operator", operator, index=0, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, horizontal=False, captions=None, label_visibility="visible")
-    #depth=st.slider("Select depth", min_value=1, max_value=2, value=1, step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+        operator=["AND", "OR"]
+        select_operator=st.radio("Select operator", operator, index=0, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, horizontal=False, captions=None, label_visibility="visible")
+        #depth=st.slider("Select depth", min_value=1, max_value=2, value=1, step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
 
-# Every form must have a submit button.
-    submitted = st.form_submit_button("Submit")
-    if submitted or st.session_state["keep_query_graphic"]:
-        st.session_state["keep_query_graphic"] = True
-        options = [v.split(':')[0] for v in options_actor]
-        options.extend([v.split(':')[0] for v in options_agreement])
-        query_indices = [adj_vertices.index(vertex) for vertex in options]
-        query_matrix,found_indices = get_query_matrix(query_indices,adj_matrix,max_depth=1,operator=select_operator)
-        display_networkx_graph(query_matrix,found_indices,adj_vertices,data_dict)
+    # Every form must have a submit button.
+        submitted = st.form_submit_button("Submit")
+        if submitted or st.session_state["keep_query_graphic"]:
+            st.session_state["keep_query_graphic"] = True
+            options = [v.split(':')[0] for v in options_actor]
+            options.extend([v.split(':')[0] for v in options_agreement])
+            query_indices = [adj_vertices.index(vertex) for vertex in options]
+            query_matrix,found_indices = get_query_matrix(query_indices,adj_matrix,max_depth=1,operator=select_operator)
+            display_networkx_graph(query_matrix,found_indices,adj_vertices,data_dict)
 
+else:
+    st.write('Please select a peace process in the Select Peace Process page.')
