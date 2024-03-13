0103e4a94895397c3bd656882a87605fd10492a1
Roy Gardner <roybowesgardner@gmail.com>
2024-03-13 10:24:19 +0000

    Update 3_Explore_Actor_Data.py

--- pages/3_Explore_Actor_Data.py
+++ pages/3_Explore_Actor_Data.py
@@ -92,7 +92,7 @@ if len(st.session_state["pp_data_dict"]) > 0:
             data_dict['vertices_dict'][pp_data_dict['pp_actor_ids'][index]][5]) for index in actor_indices]
     s = actors[0][1] + ' (' + actors[0][0] + ') and ' + actors[1][1] + ' (' + actors[1][0] + ')'
     st.caption(str(s))
-    s = 'Number of co-agreements: ' + str(actor_upper[actor_indices])
+    s = 'Number of agreements in common: ' + str(actor_upper[actor_indices])
     st.caption(str(s))
 
     f = plt.figure(figsize=(8,8))
