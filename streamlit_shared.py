10b7a4af542760bee512af9f33f7b2447da53851
Roy Gardner <roybowesgardner@gmail.com>
2024-03-18 11:03:20 +0000

    Update streamlit_shared.py

--- streamlit_shared.py
+++ streamlit_shared.py
@@ -25,7 +25,7 @@ def get_base64_of_bin_file(png_file):
 
 def build_markup_for_logo(
     png_file,
-    background_position="50% 10%",
+    background_position="10% 10%",
     margin_top="10%",
     image_width="60%",
     image_height="",
