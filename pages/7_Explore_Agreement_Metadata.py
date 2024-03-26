97fbd5b3af229918e7f8cb1a189991da8f6c2356
Roy Gardner <roybowesgardner@gmail.com>
2024-03-26 20:39:07 +0000

    Update 7_Explore_Agreement_Metadata.py

--- pages/7_Explore_Agreement_Metadata.py
+++ pages/7_Explore_Agreement_Metadata.py
@@ -19,7 +19,7 @@ if len(st.session_state["pp_data_dict"]) > 0:
     st.subheader(':blue[' + st.session_state["pp_data_dict"]['pp_name'] + ']')
 
     st.write("This page explores the relationship between actors and agreement metadata. Agreement metadata explored here are:")
-    st.text("1. Agreement date.\n\
+    st.write("1. Agreement date.\n\
 2. Agreement year.\n\
 3. Agreement stage.")
     st.write("Explore individual actor data using the form at the bottom of the page.")
