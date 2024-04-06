11da9019c489bd934d8f47ed50fdddad5d376387
Roy Gardner <roybowesgardner@gmail.com>
2024-04-06 19:44:46 +0100

    Update 8_Track_An_Actor_Over_Time.py

--- pages/8_Track_An_Actor_Over_Time.py
+++ pages/8_Track_An_Actor_Over_Time.py
@@ -13,7 +13,7 @@ st.header("Track an Actor Over Time")
 
 st.write("The video below shows UK activity in the Bosnia Peace Process. The\
          UK (CON_19) is the actor in the fixed position on the right of the screen")
-st.write("Each frame shows the UK signatory network on the date shown in the frame's title.")
+st.write("Each frame shows the UK's signatory network on the date shown in the frame's title.")
 st.write("All agreements signed on a date are shown even if the UK was not a signatory.\
          However, there are only two dates in the sequence that contain agreements not signed by\
          the UK: 1992-08-27 AGT_1170, and 1995-11-21 AGT_1289.")
