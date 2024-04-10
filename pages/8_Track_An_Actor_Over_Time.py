ec67067ffddfecf4f3fd3be6425686d751ae3ad2
Roy Gardner <roybowesgardner@gmail.com>
2024-04-10 18:43:10 +0100

    Update 8_Track_An_Actor_Over_Time.py

--- pages/8_Track_An_Actor_Over_Time.py
+++ pages/8_Track_An_Actor_Over_Time.py
@@ -22,7 +22,7 @@ st.write("The video provides a glimpse into a wealth of possibilities: changing 
 st.video('./movie.mp4', format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False)
 
 
-st.write("The video below shows Bosnia and Herzegovina (CON_0) and UK (CON_19) co-signatory engagement in the Bosnia Peace Process.")
+st.write("The video below shows Bosnia and Herzegovina (CON_0) and UK (CON_19) co-signatory engagements in the Bosnia Peace Process.")
 st.write("Bosnia and Herzegovina (CON_0) is a fixed position of the left of the frame, the UK (CON_19) on the right.")
 st.video('./movie_uk_bosnia.mp4', format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False)
 
